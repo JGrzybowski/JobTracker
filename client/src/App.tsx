d252b8823da8af0d6bca313fc6167377928b3e5e
Jan Grzybowski <JanGrzybowski@outlook.com>
2019-10-19 21:14:05 +0200

    Update FabricUI to ver 7 and simplify TS config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,49 @@
-import { initializeIcons } from '@uifabric/icons';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
-import { List } from 'office-ui-fabric-react/lib/List';
-import * as React from 'react';
+import React from 'react';
+import { Stack, Text, Link, FontWeights } from 'office-ui-fabric-react';
 
-class App extends React.Component {
-  public render() {
-    initializeIcons();
+const boldStyle = { root: { fontWeight: FontWeights.semibold } };
 
-    return (
-      <div className="App">
-        <div className="ms-Grid">
-          <div className="ms-md4">
-            <List />
-          </div>
-          <div className="ms-md8">
-            <DefaultButton
-              text='See Button'
-              primary={true}
-              href='#/components/button'>
-              I am a button.
-            </DefaultButton>          
-          </div>
-        
-        </div>
-       
-      </div>
-    );
-  }
-}
-
-export default App;
+export const App: React.FunctionComponent = () => {
+  return (
+    <Stack
+      horizontalAlign="center"
+      verticalAlign="center"
+      verticalFill
+      styles={{
+        root: {
+          width: '960px',
+          margin: '0 auto',
+          textAlign: 'center',
+          color: '#605e5c'
+        }
+      }}
+      gap={15}
+    >
+      <img
+        src="https://raw.githubusercontent.com/Microsoft/just/master/packages/just-stack-uifabric/plop-templates/src/fabric.png"
+        alt="logo"
+      />
+      <Text variant="xxLarge" styles={boldStyle}>
+        Welcome to Your UI Fabric App
+      </Text>
+      <Text variant="large">For a guide on how to customize this project, check out the UI Fabric documentation.</Text>
+      <Text variant="large" styles={boldStyle}>
+        Essential Links
+      </Text>
+      <Stack horizontal gap={15} horizontalAlign="center">
+        <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
+        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
+        <Link href="https://github.com/officeDev/office-ui-fabric-react/">Github</Link>
+        <Link href="https://twitter.com/officeuifabric">Twitter</Link>
+      </Stack>
+      <Text variant="large" styles={boldStyle}>
+        Design System
+      </Text>
+      <Stack horizontal gap={15} horizontalAlign="center">
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">Icons</Link>
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">Typography</Link>
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">Theme</Link>
+      </Stack>
+    </Stack>
+  );
+};
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,29 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { FluentCustomizations } from '@uifabric/fluent-theme';
+import { Customizer, mergeStyles } from 'office-ui-fabric-react';
+import * as serviceWorker from './serviceWorker';
+
+// Inject some global styles
+mergeStyles({
+  selectors: {
+    ':global(body), :global(html), :global(#root)': {
+      margin: 0,
+      padding: 0,
+      height: '100vh'
+    }
+  }
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Customizer {...FluentCustomizations}>
+    <App />
+  </Customizer>,
+  document.getElementById('root')
 );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
