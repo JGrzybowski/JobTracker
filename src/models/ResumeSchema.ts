fb01f4c9ee1feebf111a914e8d61c986111971cf
Jan Grzybowski <JanGrzybowski@outlook.com>
2020-10-24 02:07:06 +0200

    Resume schema mapped to section types

--- src/models/ResumeSchema.ts
+++ src/models/ResumeSchema.ts
@@ -112,6 +112,32 @@ export type WorkSectionSchema = {
   [k: string]: unknown;
 };
 
+export type VolunteerSectionSchema = {
+  /**
+   * e.g. Facebook
+   */
+  organization?: string;
+  /**
+   * e.g. Software Engineer
+   */
+  position?: string;
+  /**
+   * e.g. http://facebook.example.com
+   */
+  url?: string;
+  startDate?: Iso8601;
+  endDate?: Iso8601;
+  /**
+   * Give an overview of your responsibilities at the company
+   */
+  summary?: string;
+  /**
+   * Specify accomplishments and achievements
+   */
+  highlights?: string[];
+  [k: string]: unknown;
+};
+
 export type EducationSectionSchema = {
   /**
    * e.g. Massachusetts Institute of Technology
@@ -142,6 +168,147 @@ export type EducationSectionSchema = {
   [k: string]: unknown;
 };
 
+export type AwardsSectionSchema = {
+  /**
+   * e.g. One of the 100 greatest minds of the century
+   */
+  title?: string;
+  date?: Iso8601;
+  /**
+   * e.g. Time Magazine
+   */
+  awarder?: string;
+  /**
+   * e.g. Received for my work with Quantum Physics
+   */
+  summary?: string;
+  [k: string]: unknown;
+};
+
+export type PublicationsSectionSchema = {
+  /**
+   * e.g. The World Wide Web
+   */
+  name?: string;
+  /**
+   * e.g. IEEE, Computer Magazine
+   */
+  publisher?: string;
+  releaseDate?: Iso8601;
+  /**
+   * e.g. http://www.computer.org.example.com/csdl/mags/co/1996/10/rx069-abs.html
+   */
+  url?: string;
+  /**
+   * Short summary of publication. e.g. Discussion of the World Wide Web, HTTP, HTML.
+   */
+  summary?: string;
+  [k: string]: unknown;
+};
+
+export type SkillsSectionSchema = {
+  /**
+   * e.g. Web Development
+   */
+  name?: string;
+  /**
+   * e.g. Master
+   */
+  level?: string;
+  /**
+   * List some keywords pertaining to this skill
+   */
+  keywords?: string[];
+  [k: string]: unknown;
+};
+
+export type LanguagesSectionSchema = {
+  /**
+   * e.g. English, Spanish
+   */
+  language?: string;
+  /**
+   * e.g. Fluent, Beginner
+   */
+  fluency?: string;
+  [k: string]: unknown;
+};
+
+export type InterestsSectionSchema = {
+  /**
+   * e.g. Philosophy
+   */
+  name?: string;
+  keywords?: string[];
+  [k: string]: unknown;
+};
+
+export type ReferencesSectionSchema = {
+  /**
+   * e.g. Timothy Cook
+   */
+  name?: string;
+  /**
+   * e.g. Joe blogs was a great employee, who turned up to work at least once a week. He exceeded my expectations when it came to doing nothing.
+   */
+  reference?: string;
+  [k: string]: unknown;
+};
+
+export type ProjectsSectionSchema = {
+  /**
+   * e.g. The World Wide Web
+   */
+  name?: string;
+  /**
+   * Short summary of project. e.g. Collated works of 2017.
+   */
+  description?: string;
+  /**
+   * Specify multiple features
+   */
+  highlights?: string[];
+  /**
+   * Specify special elements involved
+   */
+  keywords?: string[];
+  startDate?: Iso8601;
+  endDate?: Iso8601;
+  /**
+   * e.g. http://www.computer.org/csdl/mags/co/1996/10/rx069-abs.html
+   */
+  url?: string;
+  /**
+   * Specify your role on this project or in company
+   */
+  roles?: string[];
+  /**
+   * Specify the relevant company/entity affiliations e.g. 'greenpeace', 'corporationXYZ'
+   */
+  entity?: string;
+  /**
+   *  e.g. 'volunteering', 'presentation', 'talk', 'application', 'conference'
+   */
+  type?: string;
+  [k: string]: unknown;
+};
+
+export type MetaSectionSchema = {
+  /**
+   * URL (as per RFC 3986) to latest version of this document
+   */
+  canonical?: string;
+  /**
+   * A version field which follows semver - e.g. v1.0.0
+   */
+  version?: string;
+  /**
+   * Using ISO 8601 with YYYY-MM-DDThh:mm:ss
+   */
+  lastModified?: string;
+  [k: string]: unknown;
+};
+
 export interface ResumeSchema {
   /**
    * link to the version of the schema that can validate the resume
@@ -149,184 +316,35 @@ export interface ResumeSchema {
   $schema?: string;
   basics?: BasicsSectionSchema;
   work?: WorkSectionSchema[];
-  volunteer?: {
-    /**
-     * e.g. Facebook
-     */
-    organization?: string;
-    /**
-     * e.g. Software Engineer
-     */
-    position?: string;
-    /**
-     * e.g. http://facebook.example.com
-     */
-    url?: string;
-    startDate?: Iso8601;
-    endDate?: Iso8601;
-    /**
-     * Give an overview of your responsibilities at the company
-     */
-    summary?: string;
-    /**
-     * Specify accomplishments and achievements
-     */
-    highlights?: string[];
-    [k: string]: unknown;
-  }[];
+  volunteer?: VolunteerSectionSchema[];
   education?: EducationSectionSchema[];
   /**
    * Specify any awards you have received throughout your professional career
    */
-  awards?: {
-    /**
-     * e.g. One of the 100 greatest minds of the century
-     */
-    title?: string;
-    date?: Iso8601;
-    /**
-     * e.g. Time Magazine
-     */
-    awarder?: string;
-    /**
-     * e.g. Received for my work with Quantum Physics
-     */
-    summary?: string;
-    [k: string]: unknown;
-  }[];
+  awards?: AwardsSectionSchema[];
   /**
    * Specify your publications through your career
    */
-  publications?: {
-    /**
-     * e.g. The World Wide Web
-     */
-    name?: string;
-    /**
-     * e.g. IEEE, Computer Magazine
-     */
-    publisher?: string;
-    releaseDate?: Iso8601;
-    /**
-     * e.g. http://www.computer.org.example.com/csdl/mags/co/1996/10/rx069-abs.html
-     */
-    url?: string;
-    /**
-     * Short summary of publication. e.g. Discussion of the World Wide Web, HTTP, HTML.
-     */
-    summary?: string;
-    [k: string]: unknown;
-  }[];
+  publications?: PublicationsSectionSchema[];
   /**
    * List out your professional skill-set
    */
-  skills?: {
-    /**
-     * e.g. Web Development
-     */
-    name?: string;
-    /**
-     * e.g. Master
-     */
-    level?: string;
-    /**
-     * List some keywords pertaining to this skill
-     */
-    keywords?: string[];
-    [k: string]: unknown;
-  }[];
+  skills?: SkillsSectionSchema[];
   /**
    * List any other languages you speak
    */
-  languages?: {
-    /**
-     * e.g. English, Spanish
-     */
-    language?: string;
-    /**
-     * e.g. Fluent, Beginner
-     */
-    fluency?: string;
-    [k: string]: unknown;
-  }[];
-  interests?: {
-    /**
-     * e.g. Philosophy
-     */
-    name?: string;
-    keywords?: string[];
-    [k: string]: unknown;
-  }[];
+  languages?: LanguagesSectionSchema[];
+  interests?: InterestsSectionSchema[];
   /**
    * List references you have received
    */
-  references?: {
-    /**
-     * e.g. Timothy Cook
-     */
-    name?: string;
-    /**
-     * e.g. Joe blogs was a great employee, who turned up to work at least once a week. He exceeded my expectations when it came to doing nothing.
-     */
-    reference?: string;
-    [k: string]: unknown;
-  }[];
+  references?: ReferencesSectionSchema[];
   /**
    * Specify career projects
    */
-  projects?: {
-    /**
-     * e.g. The World Wide Web
-     */
-    name?: string;
-    /**
-     * Short summary of project. e.g. Collated works of 2017.
-     */
-    description?: string;
-    /**
-     * Specify multiple features
-     */
-    highlights?: string[];
-    /**
-     * Specify special elements involved
-     */
-    keywords?: string[];
-    startDate?: Iso8601;
-    endDate?: Iso8601;
-    /**
-     * e.g. http://www.computer.org/csdl/mags/co/1996/10/rx069-abs.html
-     */
-    url?: string;
-    /**
-     * Specify your role on this project or in company
-     */
-    roles?: string[];
-    /**
-     * Specify the relevant company/entity affiliations e.g. 'greenpeace', 'corporationXYZ'
-     */
-    entity?: string;
-    /**
-     *  e.g. 'volunteering', 'presentation', 'talk', 'application', 'conference'
-     */
-    type?: string;
-    [k: string]: unknown;
-  }[];
+  projects?: ProjectsSectionSchema[];
   /**
    * The schema version and any other tooling configuration lives here
    */
-  meta?: {
-    /**
-     * URL (as per RFC 3986) to latest version of this document
-     */
-    canonical?: string;
-    /**
-     * A version field which follows semver - e.g. v1.0.0
-     */
-    version?: string;
-    /**
-     * Using ISO 8601 with YYYY-MM-DDThh:mm:ss
-     */
-    lastModified?: string;
-    [k: string]: unknown;
-  };
+  meta?: MetaSectionSchema;
 }
